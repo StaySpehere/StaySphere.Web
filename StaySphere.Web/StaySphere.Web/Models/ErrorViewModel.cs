b1ca3c67b804d0750a9e7110a82b657c5e1b0c0b
diyorzz <128776299+diyorzz@users.noreply.github.com>
2024-01-17 17:44:20 +0500

    Initial_Create

---
+++ StaySphere.Web/StaySphere.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StaySphere.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
